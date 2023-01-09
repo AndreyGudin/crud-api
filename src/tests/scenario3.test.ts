a6c53db2eb7aa783e883052d0522ed932eba2e49
Andrey Gudin <aeon150791@gmail.com>
2023-01-09 21:18:49 +0400

    feat add test scenario 3

---
+++ src/tests/scenario3.test.ts
@@ -0,0 +1,45 @@
+import supertest from "supertest";
+
+import { User } from "../types/types";
+import createSingleServer from "../server";
+
+const users: User[] = [];
+
+const app = createSingleServer(users);
+
+describe("Test scenario 3", () => {
+  test('Expecting GET response with status 404 if route is invalid', async () => {
+    const response = await supertest(app)
+      .get('/api/test/test')
+      .set('Accept', 'application/json')
+    expect(response.status).toEqual(404);
+  })
+  test('Expecting GET response with status 400 if UUID is invalid', async () => {
+    const response = await supertest(app)
+      .get('/api/users/test')
+      .set('Accept', 'application/json')
+    expect(response.status).toEqual(400);
+  })
+  test('Expecting POST response with status 400 and correct message if body is invalid', (done) => {
+    const user = {
+      "hobbies": [
+        "fishing",
+        "cats"
+      ]
+    };
+    supertest(app)
+      .post("/api/users/")
+      .expect("Content-Type", /json/)
+      .send(JSON.stringify(user))
+      .expect(400)
+      .expect((res) => {   
+        expect(res.body.title).toEqual('Validation is failed');
+        expect(res.body.message).toEqual('Request body is not valid');   
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  })
+
+})
